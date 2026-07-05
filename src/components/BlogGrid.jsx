59f1991b36d0ac26e046cca84f2c5261b4a37e8c
Josh Bello <Jb230913@live.com>
2026-07-05 21:43:03 +0100

    update - added logic to display Component

--- src/components/BlogGrid.jsx
+++ src/components/BlogGrid.jsx
@@ -13,7 +13,7 @@ function BlogGrid(props) {
 
     return (
         <>
-            grid
+            Blog grid
             <section data-template="blogGrid" data-view="default" className='c-blog-grid'>
                 <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
 
