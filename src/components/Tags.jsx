ae3109fe258875b31207d647ce7e75900af5347d
Josh Bello <Jb230913@live.com>
2026-07-05 23:15:07 +0100

    update - add static responsive UI for Tag Component

---
+++ src/components/Tags.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Tags(props) {
+    return (
+        <>
+            <h1>Tags</h1>
+            <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+                <div class="flex flex-wrap gap-6 ">
+                    <a class="py-3 px-3 bg-amber-400 rounded-3xl  md:w-auto">Internet Of Things</a>
+                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Economic</a>
+                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Finance</a>
+                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Politics</a>
+                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Basketball</a>
+                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Socioliogy</a>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Tags;
