c1ad4d7b5a93e978bd06567e5726d38818bbfa3f
Josh Bello <Jb230913@live.com>
2026-07-06 18:24:14 +0100

    update - added href attributes

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -6,12 +6,12 @@ function Tags(props) {
             <h1>Tags</h1>
             <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
                 <div class="flex flex-wrap gap-6 ">
-                    <a class="py-3 px-3 bg-amber-400 rounded-3xl  md:w-auto">Internet Of Things</a>
-                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Economic</a>
-                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Finance</a>
-                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Politics</a>
-                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Basketball</a>
-                    <a class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Socioliogy</a>
+                    <a href='' class="py-3 px-3 bg-amber-400 rounded-3xl  md:w-auto">Internet Of Things</a>
+                    <a href='' class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Economic</a>
+                    <a href='' class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Finance</a>
+                    <a href='' class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Politics</a>
+                    <a href='' class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Basketball</a>
+                    <a href='' class="py-3 px-3  bg-amber-400 rounded-3xl md:w-auto">Socioliogy</a>
                 </div>
             </div>
 
