0bc66970c1d58043f87a00243a9f1150e252e5d5
Josh Bello <Jb230913@live.com>
2026-07-07 10:36:50 +0100

    updated - packages installed + add blog post layout

---
+++ src/components/layout/blog.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+function Blog({ children }) {
+    return (
+        <div>
+            <Header />
+            <main>
+
+                {children}
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default Blog;
