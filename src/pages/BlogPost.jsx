5d0170d33b941dbee54ef3f92bbac22c557a7fbd
Josh Bello <Jb230913@live.com>
2026-07-09 10:42:48 +0100

    update - logic for getting data and passing by prop to component

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import BlogArticle from '../components/BlogArticle';
+import { useParams } from "react-router";
+import { getArticleById } from '../utils/functions';
+import PageNotFound from './PageNotFound';
 function BlogPost(props) {
+    const { id, slug } = useParams();
+
+    const article = getArticleById(id);
+    // console.log(article);
+    if (!article) {
+        return <PageNotFound />;
+    }
     return (
         <>
-            <BlogArticle />
+            <BlogArticle article={article} />
         </>
     );
 }
