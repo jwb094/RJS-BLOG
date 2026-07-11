07835cd62fb589fdc13511b49eb58b12082270c9
Josh Bello <Jb230913@live.com>
2026-07-11 13:55:30 +0100

    update - add logic to create and display schema

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -11,8 +11,43 @@ function BlogPost(props) {
     if (!article) {
         return <PageNotFound />;
     }
+
+
+    const schema = {
+        "@context": "https://schema.org",
+        "@type": "BlogPosting",
+        headline: article.title,
+        description: article.description,
+        image: [
+            `https://example.com${article.featuredImage}`
+        ],
+        author: {
+            "@type": "Person",
+            name: article.author.name
+        },
+        publisher: {
+            "@type": "Organization",
+            name: "StoryPhrase",
+            logo: {
+                "@type": "ImageObject",
+                url: "https://example.com/logo.png"
+            }
+        },
+        datePublished: article.publishedDate,
+        dateModified: article.modifiedDate,
+        mainEntityOfPage: {
+            "@type": "WebPage",
+            "@id": `/blog/id/${article.slug}`
+        }
+    };
     return (
         <>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(schema),
+                }}
+            />
             <BlogArticle article={article} />
         </>
     );
