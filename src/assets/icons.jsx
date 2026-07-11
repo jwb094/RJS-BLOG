eade694c9c8cc0734c2e5b0e97d28429bae5a0db
Josh Bello <Jb230913@live.com>
2026-07-11 13:44:09 +0100

    update - class => className

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -26,14 +26,14 @@ export const TwitterIcon = (props) => (
 
 
 export const DateTimeIcon = (props) => {
-    return (<svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" classNameName="bi bi-calendar" viewBox="0 0 16 16">
+    return (<svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" className="bi bi-calendar" viewBox="0 0 16 16">
         <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
     </svg>)
 }
 
 
 export const CatgeoriesIcon = (props) => {
-    return (<svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" classNameName="bi bi-folder" viewBox="0 0 16 16">
+    return (<svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" className="bi bi-folder" viewBox="0 0 16 16">
         <path d="M.54 3.87.5 3a2 2 0 0 1 2-2h3.672a2 2 0 0 1 1.414.586l.828.828A2 2 0 0 0 9.828 3h3.982a2 2 0 0 1 1.992 2.181l-.637 7A2 2 0 0 1 13.174 14H2.826a2 2 0 0 1-1.991-1.819l-.637-7a2 2 0 0 1 .342-1.31zM2.19 4a1 1 0 0 0-.996 1.09l.637 7a1 1 0 0 0 .995.91h10.348a1 1 0 0 0 .995-.91l.637-7A1 1 0 0 0 13.81 4zm4.69-1.707A1 1 0 0 0 6.172 2H2.5a1 1 0 0 0-1 .981l.006.139q.323-.119.684-.12h5.396z" />
     </svg>)
 }
@@ -56,14 +56,14 @@ export const CatgeoriesIcon = (props) => {
 
 
 export const TagsIcon = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" classNameName="bi bi-tags" viewBox="0 0 16 16">
+    <svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" className="bi bi-tags" viewBox="0 0 16 16">
         <path d="M3 2v4.586l7 7L14.586 9l-7-7zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586z" />
         <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1m0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3M1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1z" />
     </svg>
 )
 
 export const PencilIcon = (props) => {
-    return (<svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+    return (<svg xmlns="http://www.w3.org/2000/svg" width={props.width} height={props.height} fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
     </svg>)
 }
