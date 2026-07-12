730b67afb528220542cf918f18b6e712c0214579
Josh Bello <Jb230913@live.com>
2026-07-12 23:02:46 +0100

    update  - import partial scss file + removal of unneeded HTML tags and edit utility classes - BlogArticle

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -2,40 +2,42 @@ import React from 'react';
 import {
     TwitterIcon, LinkedInIcon, FacebookIcon, InstagramIcon
 } from '../assets/icons';
+import "../assets/styles/blogCard.scss"
 function AuthorCard({ author }) {
+    console.log(window.location.pathname.includes("author"));
     return (
         <>
             <section data-template="author-card" data-view="default" className='c-author-card'>
-                <div className="mx-auto max-w-7xl px-0 py-4 sm:px-6 lg:px-8">
+                <div className="mx-auto max-w-7xl   md:py-4 sm:px-6 lg:px-0">
 
                     <div className="flex flex-col md:flex-row flex-wrap   ">
-                        {/* <div class="border-4 border-top border-indigo-500 ..."></div> */}
-                        {/* <div className='border-t-4 border-solid border-black'></div> */}
                         <div className='author-block flex flex-row justify-start | gap-4'>
-                            <div className="author-img w-auto">
-
-                                <img className="w-32 h-32  md:h-48 md:w-48 object-cover ..." src={author.avatar} />
+                            <div className="author-img w-1/3">
+                                <img className="w-auto h-auto md:w-32 md:h-32  md:h-48 md:w-48 object-cover" src={author.avatar} />
                             </div>
-                            <div className='w-3/6 flex flex-col justify-between'>
-                                <h2 className='text-lg'>{author.name}
-                                </h2>
+                            <div className='w-2/3 md:w-3/6 flex flex-col justify-between'>
+                                {!window.location.pathname.includes("author") && (
+                                    <h2 className='text-lg'>{author.name}
+                                    </h2>
+                                )}
                                 <p>{author.description}</p>
-                                <ul className='social flex flex-row md:flex-row justify-between  mt-3 gap-3'>
-                                    <li>
-                                        <a href={author.social.facebook}><FacebookIcon width={20} height={20} />
-                                        </a>
-                                    </li>
-                                    <li><a href={author.social.instagram}><InstagramIcon width={20} height={20} /></a></li>
-                                    <li><a href={author.social.linkedin}><LinkedInIcon width={20} height={20} /></a></li>
-                                    <li><a href={author.social.x}><TwitterIcon width={20} height={20} /></a></li>
-                                </ul>
+                                {window.location.pathname.includes("author") !== false && (
+                                    <ul className='social flex flex-row md:flex-row justify-between  mt-3 gap-3'>
+                                        <li>
+                                            <a href={author.social.facebook}><FacebookIcon width={20} height={20} />
+                                            </a>
+                                        </li>
+                                        <li><a href={author.social.instagram}><InstagramIcon width={20} height={20} /></a></li>
+                                        <li><a href={author.social.linkedin}><LinkedInIcon width={20} height={20} /></a></li>
+                                        <li><a href={author.social.x}><TwitterIcon width={20} height={20} /></a></li>
+                                    </ul>
+                                )}
                                 {!window.location.pathname.includes("author") && (
                                     <a href={`/blog/author/${author.id}/${author.slug}`}> Go To Profile</a>
                                 )}
 
                             </div>
                         </div>
-                        {/* <div className='border-t-4 border-solid border-black'></div> */}
                     </div >
                 </div >
             </section >
--- src/components/BlogArticle.jsx
+++ src/components/BlogArticle.jsx
@@ -11,73 +11,69 @@ import {
 } from '../assets/icons';
 import Tags from '../components/Tags'
 import AuthorCard from '../components/AuthorCard'
+import "../assets/styles/blogArticle.scss"
 function BlogArticle({ article }) {
     return (
         <>
 
-            <section data-template="blog-post" data-view="default" className='c-blog-post'>
-                <div className="mx-auto max-w-7xl px-6 py-16 sm:px-6 lg:px-8">
+            <article data-template="blog-post" data-view="default" className='c-blog-post'>
+                <div className="mx-auto max-w-7xl px-6 py-8 md:py-16 sm:px-6 lg:px-8">
                     {/* mobile blog tite and intro */}
-                    <div className=' h-auto hidden md:block'>
-                        <h1 className='text-6xl'>{article.title}</h1>
-                        <p className='text-3xl | py-4'>{article.subHeading}</p>
-                        <ul className='flex flex-row justify-baseline gap-x-12'>
-                            <li className='flex gap-x-2'> <DateTimeIcon width={20} height={20} /> {article.publishedDate}</li>
-                            <li className='flex gap-x-2'> <ThumbsUpIcon width={20} height={20} /> {article.likes}</li>
-                            <li className='flex gap-x-2'> <CommentsIcon width={20} height={20} /> {article.comments}</li>
-                            <li className='flex gap-x-2'> <ReadingTimeIcon width={20} height={20} /> {article.readingTime}&nbsp;mins</li>
-                            <li className='flex gap-x-2'>
-                                <CatgeoriesIcon width={20} height={20} />
-                                <a href={`/blog/category/${article.cat_slug}`}>
-                                    {article.category.charAt(0).toUpperCase() + article.category.slice(1)}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    {/* <div className=' h-auto '> */}
+                    <h1 className='text-2xl md:text-6xl'>{article.title}</h1>
+                    <p className='md:text-3xl | py-4'>{article.subHeading}</p>
+                    <ul className='flex flex-row justify-baseline gap-x-12 | hidden md:flex'>
+                        <li className='flex gap-x-2'> <DateTimeIcon width={20} height={20} /> {article.publishedDate.split("-").reverse().join(".")}</li>
+                        <li className='flex gap-x-2'> <ThumbsUpIcon width={20} height={20} /> {article.likes}</li>
+                        <li className='flex gap-x-2'> <CommentsIcon width={20} height={20} /> {article.comments}</li>
+                        <li className='flex gap-x-2'> <ReadingTimeIcon width={20} height={20} /> {article.readingTime}&nbsp;mins</li>
+                        <li className='flex gap-x-2'>
+                            <CatgeoriesIcon width={20} height={20} />
+                            <a href={`/blog/category/${article.cat_slug}`}>
+                                {article.category.charAt(0).toUpperCase() + article.category.slice(1)}
+                            </a>
+                        </li>
+                    </ul>
+                    {/* </div> */}
                     {/* AuthorCard */}
-                    <div className=' h-auto hidden md:block'>
-                        {/* <a href={`/blog/author/${article.author.id}/${article.author.slug}`}> */}
-                        <AuthorCard author={article.author} />
-                        {/* </a> */}
-                    </div>
-                    {/* blog main img and caption */}
-                    <div className='c-blog-post-main-img | flex flex-row justify-center | py-6 '>
-                        <figure>
-                            <img class="  md:h-auto md:w-auto object-cover ..." src={article.featuredImage} alt={article.featuredImageAlt} />
-                            <figcaption className=' mt-2 '>{article.imageCaption}</figcaption>
-                        </figure>
-                    </div>
-                    {/* author - mobile */}
-                    <div className=' h-auto md:hidden | mt-5 flex flex-col gap-y-5'>
-                        {/* <a href={`/blog/author/${article.author.id}/${article.author.slug}`}> */}
-                        <AuthorCard author={article.author} />
-                        {/* </a> */}
-                    </div>
-                    {/* mobile title and intro */}
-                    <div className=' h-auto md:hidden | mt-5 flex flex-col gap-y-5'>
-                        <h1>{article.title}</h1>
-                        <p>{article.subHeading}</p>
+                    {/* <div className=' h-auto'> */}
 
-                        <ul className='flex flex-row justify-between'>
-                            <li className='flex gap-x-2'> <DateTimeIcon width={20} height={20} /> {article.publishedDate}</li>
-                            <li className='flex gap-x-2'> <ThumbsUpIcon width={20} height={20} /> {article.likes}</li>
-                            <li className='flex gap-x-2'> <CommentsIcon width={20} height={20} /> {article.comments}</li>
-                            <li className='flex gap-x-2'> <ReadingTimeIcon width={20} height={20} /> {article.readingTime}&nbsp;mins</li>
-                            <li className='flex gap-x-2'> <CatgeoriesIcon width={20} height={20} />
-                                <a href={`/blog/category/${article.cat_slug}`}>
-                                    {article.category.charAt(0).toUpperCase() + article.category.slice(1)}
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    <AuthorCard author={article.author} />
+
+                    {/* </div> */}
+                    {/* blog main img and caption */}
+                    {/* <div > */}
+                    <figure className='c-blog-post-main-img | py-6 '>
+                        <img class="  object-cover ..." height="450" src={article.featuredImage} alt={article.featuredImageAlt} />
+                        <figcaption className=' mt-2 '>{article.imageCaption}</figcaption>
+                    </figure>
+                    {/* </div> */}
+                    {/* author - mobile md:h-auto md:w-auto */}
+                    {/* <div className=' h-auto md:hidden | mt-5 flex flex-col gap-y-5'> */}
+                    {/* <a href={`/blog/author/${article.author.id}/${article.author.slug}`}> */}
+                    {/* <AuthorCard author={article.author} /> */}
+                    {/* </a> */}
+                    {/* </div> */}
+                    {/* mobile title and intro */}
+                    {/* <div className=' h-auto md:hidden | mt-5 flex flex-col gap-y-5'> */}
+                    <ul className='flex flex-row flex-wrap justify-between | gap-y-4 |  h-auto md:hidden '>
+                        <li className='flex gap-x-2'> <DateTimeIcon width={20} height={20} /> {article.publishedDate.split("-").reverse().join(".")}</li>
+                        <li className='flex gap-x-2'> <ThumbsUpIcon width={20} height={20} /> {article.likes}</li>
+                        <li className='flex gap-x-2'> <CommentsIcon width={20} height={20} /> {article.comments}</li>
+                        <li className='flex gap-x-2'> <ReadingTimeIcon width={20} height={20} /> {article.readingTime}&nbsp;mins</li>
+                        <li className='flex gap-x-2'> <CatgeoriesIcon width={20} height={20} />
+                            <a href={`/blog/category/${article.cat_slug}`}>
+                                {article.category.charAt(0).toUpperCase() + article.category.slice(1)}
+                            </a>
+                        </li>
+                    </ul>
+                    {/* </div> */}
                     {/* content */}
-                    <div className="c-blog-post-main-content  flex flex-col  gap-y-3 | mt-5">
-                        <div
-                            dangerouslySetInnerHTML={{
-                                __html: article.content
-                            }}
-                        />
-                    </div>
+                    <section className="c-blog-post-main-content  flex flex-col  gap-y-3 | mt-5"
+                        dangerouslySetInnerHTML={{
+                            __html: article.content
+                        }}
+                    />
                     {/* Tags */}
                     <Tags topics={article.topicsTags} />
                     {/* Share */}
@@ -92,7 +88,7 @@ function BlogArticle({ article }) {
                         </ul>
                     </div>
                 </div>
-            </section>
+            </article >
         </>
     );
 }
