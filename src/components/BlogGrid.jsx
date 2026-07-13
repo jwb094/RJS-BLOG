7a9680425c4731a13bd15e7b3db6f48ad189df70
Josh Bello <Jb230913@live.com>
2026-07-13 16:21:22 +0100

    removal - unneeded html elements add - import BlogCard-hover.scss

--- src/components/BlogGrid.jsx
+++ src/components/BlogGrid.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import BlogCard from './BlogCard';
 // import { mostRecentPost } from '../utils/functions';
-
+import "../assets/styles/BlogCard-hover.scss"
 function BlogGrid({ content }) {
     return (
         <>
             <section data-template="blogGrid" data-view="default" className='c-blog-grid'>
-                <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+                <div className="flex justify-center mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
 
-                    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                        {content.map((post, index) => (
-                            <div className="w-full " key={index}>
-                                <BlogCard key={post.id} post={post} />
-                            </div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 | cards-container">
+                        {content.map((post) => (
+                            <BlogCard key={post.id} post={post} />
                         ))}
                     </div>
                 </div>
