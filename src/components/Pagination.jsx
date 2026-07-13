283f65e8686731462bfe0341fd8a183543f95b11
Josh Bello <Jb230913@live.com>
2026-07-13 16:23:08 +0100

    update - utility class to first && Last

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -17,7 +17,7 @@ function Pagination(props) {
         <section data-template="pagination" data-view="default" className='c-pagination'>
             <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
                 <div className="flex justify-center space-x-1">
-                    <button className="rounded-md border border-slate-300 py-2 px-3 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                    <button className="hidden md:flex rounded-md border border-slate-300 py-2 px-3 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
                         disabled={props.page === 1}
                         onClick={() => props.onPageChange(1)}>
                         First
@@ -33,7 +33,7 @@ function Pagination(props) {
                     >
                         Next
                     </button>
-                    <button className="min-w-9 rounded-md border border-slate-300 py-2 px-3 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                    <button className="hidden md:flex min-w-9 rounded-md border border-slate-300 py-2 px-3 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-white hover:bg-slate-800 hover:border-slate-800 focus:text-white focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-white active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
                         onClick={() => props.onPageChange(props.totalPages)}
                         disabled={props.page === props.totalPages}
                     >
