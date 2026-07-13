2fe5bf224993b4a48b93bc52342c88d3bdc351a8
Josh Bello <Jb230913@live.com>
2026-07-13 16:08:27 +0100

    update - key attribute added + utility class for <a> removal of html elements

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-
     TagsIcon
 } from '../assets/icons';
 function Tags({ topics }) {
@@ -8,19 +7,16 @@ function Tags({ topics }) {
         <>
             <h1 className='mt-12'>Tags</h1>
             <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
-                <div class="flex flex-wrap gap-6 w-full">
-                    <ul className='flex flex-col md:flex-row gap-y-4  md:gap-x-5'>
-                        {topics.map((topic) => (
-                            <a href={`/blog/tag/${topic.slug}`} class=" bg-amber-400 rounded-3xl  md:w-auto  h-max ">
-                                <li class="flex flex-row py-3 px-3 bg-amber-400 rounded-3xl  md:w-auto  gap-x-4">
-                                    <TagsIcon width={24} height={24} />{topic.name}
-                                </li>
-                            </a>
-                        ))}
-                    </ul>
-                </div>
+                <ul className='flex flex-row  flex-wrap   gap-6  md:gap-x-5 w-full'>
+                    {topics.map((topic) => (
+                        <a key={topic.id} href={`/blog/tag/${topic.slug}`} class=" bg-amber-400 rounded-3xl max-w-max   md:w-auto  h-max ">
+                            <li className="flex flex-row py-3 px-3 bg-amber-400 rounded-3xl  md:w-auto  gap-x-4">
+                                <TagsIcon width={24} height={24} />{topic.name}
+                            </li>
+                        </a>
+                    ))}
+                </ul>
             </div>
-
         </>
     );
 }
