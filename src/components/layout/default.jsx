dce09c1ea42527c4b809d45b80034e85db02e846
Josh Bello <Jb230913@live.com>
2026-07-14 11:50:47 +0100

    updatev- added helemetPRovide Component to default layout Add - new resuable meta tags component for pages

--- src/components/layout/default.jsx
+++ src/components/layout/default.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer';
 import { Outlet } from 'react-router';
+import { HelmetProvider } from 'react-helmet-async';
 function Default({ children }) {
     return (
         <>
-            <main>
-                <Header />
-                <Outlet />
-                <Footer />
-            </main>
+            <HelmetProvider>
+                <main>
+                    <Header />
+                    <Outlet />
+                    <Footer />
+                </main>
+            </HelmetProvider>
         </>
     );
 }
